437cf6fba7b43a25261cb8bb6e6be8eb435e837b
antupauldevp <160340203+antupauldevp@users.noreply.github.com>
2024-05-06 00:54:16 +0600

    Add files via upload

---
+++ info.js
@@ -0,0 +1,27 @@
+
+
+let a = prompt('enter your name')
+if(a >= 80 && a<=100){
+    document.write("A+")
+}
+if(a >=75 && a<=79){
+    document.write("A");
+}
+if(a >=70 && a<=74){
+    document.write("A-");
+}
+if(a >=65 && a<=69){
+    document.write("B+");
+}
+if(a >=60 && a<=64){
+    document.write("B");
+}
+if(a >=55 && a<=59){
+    document.write("B-");
+}
+if(a >=50 && a<=54){
+    document.write("c+");
+}
+if(a >=45 && a<=49){
+    document.write("c");
+}
